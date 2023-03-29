b698926d52229fccf54c39e305b249ce8eed0321
Adebanjo Favour <favouradebanjo2@gmail.com>
2023-03-29 20:11:05 +0100

    i changed my style config

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from 'react';
+import  {useState} from 'react';
+
 
 const Hero = () => {
+    let myName = "Joshua";
+    let mySchool = "SQI";
+    let number = 8
+
+        const [first, setfirst] = useState(0)
+
+        const increment = () => {
+            setfirst(first+1)
+            console.log(first);
+        }
+        const decrement = () => {
+            setfirst(first-1)
+            console.log(first);
+        }
+        const restart = () => {
+            setfirst(0)
+            console.log(first);
+        }
+
+        let allStudents = [
+          {name: "Shola", school:"SQI"},
+          {name: "Mike", school:"Lautech"},
+          {name: "Elijah", school:"Poly"}
+        ]
+
   return (
-    <div>
-      <h1 className='para'>Hello world</h1>
-    </div>
-  )
+      <>
+      {number == 8 ? <div>even number</div> : <div>Odd number</div>}
+      <button onClick={increment} className='btn btn-warning mx-2'>Increment</button>
+      <button onClick={decrement} className='btn btn-danger'>Decrement</button>
+      <button onClick={restart} className='btn btn-danger mx-2'>Restart</button>
+        <div>{first}</div>
+
+
+        {
+          allStudents.map((item)=>(
+            <>
+            <div>{item.name} {item.school}</div>
+            </>
+          ))
+        }
+      </>
+  );
 }
 
-export default Hero
+export default Hero;
